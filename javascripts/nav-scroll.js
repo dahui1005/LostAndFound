cdfe60549a7eccf233e8a26d119ea2846d1d459a
dahuiLim <s18056@gsm.hs.kr>
2020-01-06 10:10:40 +0900

    네비 달은거 다시

--- javascripts/nav-scroll.js
+++ javascripts/nav-scroll.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
             $(function() {
 
                 $(window).scroll(function() {
-                    if ($(this).scrollTop() > 30) {
-                        $('#head').fadeOut();
-                    } else {
-                        $('#head').fadeIn();
-                        
-                    }
-                    if ($(this).scrollTop() > 50) {
+//                    if ($(this).scrollTop() > 30) {
+//                        $('#head').fadeOut();
+//                    } else {
+//                        $('#head').fadeIn();
+//                        
+//                    }
+                    if ($(this).scrollTop() > 80) {
                         $("#scrolled").fadeIn();
                     } else {
                         $("#scrolled").fadeOut();
