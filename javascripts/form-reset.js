fc148738bc69cb3949e667afc4e2c23be7e6d232
dahuiLim <s18056@gsm.hs.kr>
2020-01-06 11:40:30 +0900

    체크박스, js추가, 폼리셋js수정

--- javascripts/form-reset.js
+++ javascripts/form-reset.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
         $("form").each(function () {
             if (this.id == "content-form") {
                 this.reset();
+                $("#idham").attr("disabled", true);
+                $("#idpwd").attr("disabled", true);
                 $(".upload-display").empty();
             };
         });
